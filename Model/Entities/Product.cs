8fa11e833ae47c6ce44b8fbe0572815882a3ef4f
RaniaG <raniagml12@gmail.com>
2019-06-18 15:55:20 +0200

    created Product entity

---
+++ Model/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Entities
+{
+    class Product
+    {
+        int id;
+        string name;
+        float price;
+        float discount;
+        int quantity;
+        string description;
+        string[] images;
+        string terms;
+        float rate;
+        
+    }
+}
