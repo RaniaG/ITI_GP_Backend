bd4b30c9a24231ed2b64547fa97323bc4147678b
BassantFahmy <bassantfahmy@gmail.com>
2019-06-20 15:50:00 +0200

    RoleManager and asign userRole and sellerRole

--- api/Controllers/HomeController.cs
+++ api/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using api.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +12,24 @@ namespace api.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ApplicationRoleManager RoleManager
         {
+            get
+            {
+                return HttpContext.GetOwinContext().Get<ApplicationRoleManager>();
+            }
+        }
+        public async Task<ActionResult> Index()
+        {
+           
             ViewBag.Title = "Home Page";
+            /*
+            IdentityRole user = new IdentityRole("user");
+            IdentityRole seller = new IdentityRole("seller");
 
+            var u = await RoleManager.CreateAsync(user);
+            var s = await RoleManager.CreateAsync(seller);
+            */
             return View();
         }
     }
